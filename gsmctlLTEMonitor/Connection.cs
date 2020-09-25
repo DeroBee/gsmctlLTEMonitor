6b2f2b2eda43d58ba3e159120d629d693b9a3359
Erik B <40665617+DeroBee@users.noreply.github.com>
2020-09-25 21:03:47 +0200

    Added a program.cs file for the main() function. Prototyped inital Connection methods

--- gsmctlLTEMonitor/Connection.cs
+++ gsmctlLTEMonitor/Connection.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace gsmctlLTEMonitor
 {
-	class Connection
+	static class Connection
 	{
+
+		/// <summary>
+		/// Open a connection to the SSH router.
+		/// </summary>
+		/// <returns>
+		/// <list type="bullet">
+		/// <item><term>true</term><description>A connection was successfully established</description></item>
+		/// <item><term>false</term><description>No connection could be established.</description></item></list>
+		/// </returns>
+		static public bool Connect()
+		{
+			throw new NotImplementedException();
+		}
+
+
+		/// <summary>
+		/// Gets SINR RSRQ RSRQ and GSM Signal Level from the router
+		/// </summary>
+		/// <returns>A datarow containing all the requested data</returns>
+		static public DataRow GetData()
+		{
+			throw new NotImplementedException();
+		}
+
+
+		/// <summary>
+		/// Sends the command via the SSH connection to the router and waits for the output
+		/// </summary>
+		/// <param name="command">SSH command to the router</param>
+		/// <returns>Router output </returns>
+		static public string RunCommand(string command) 
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
--- gsmctlLTEMonitor/program.cs
+++ gsmctlLTEMonitor/program.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace gsmctlLTEMonitor
 {
-	class DataHandling
+	class program
 	{
-
 	}
 }
